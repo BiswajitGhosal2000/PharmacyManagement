6da849331ad004017a70d7bbd4d4260c7b7023c6
gauravcs007 <gauravkumar12y@gmail.com>
2023-02-23 22:15:18 +0530

    Admin ,Employee from Gaurav

--- src/java/com/exavalu/pharmacymgmt/models/Admin.java
+++ src/java/com/exavalu/pharmacymgmt/models/Admin.java
@@ -7,8 +7,6 @@ package com.exavalu.pharmacymgmt.models;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import java.io.Serializable;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Map;
 import org.apache.log4j.Logger;
 import org.apache.struts2.dispatcher.ApplicationMap;
@@ -18,87 +16,45 @@ import org.apache.struts2.interceptor.SessionAware;
 
 /**
  *
- * @author gaurav
+ * @author gaura
  */
-public class Admin extends ActionSupport implements ApplicationAware, SessionAware, Serializable {
-
-    public static Admin admin = null;
+public class Admin extends ActionSupport implements ApplicationAware, SessionAware, Serializable{
+     
+ public static Admin admin = null;
 
     public static Admin getInstance() {
         if (admin == null) {
-            admin = new Admin();
+            return new Admin();
+        } else {
+            return admin;
         }
-
-        return admin;
     }
+    
+    private String emailId;
+    private String password;
+    private String adminName;
+    private String phoneNumber;
 
-    private String emailId, password,adminName,phoneNumber;
     
     private ApplicationMap map = (ApplicationMap) ActionContext.getContext().getApplication();
     private SessionMap<String, Object> sessionMap = (SessionMap) ActionContext.getContext().getSession();
     private static final Logger log = Logger.getLogger(Admin.class);
+    
 
     @Override
     public void setApplication(Map<String, Object> application) {
-        map = (ApplicationMap) application;
+         map = (ApplicationMap) application;
     }
 
     @Override
     public void setSession(Map<String, Object> session) {
-        sessionMap = (SessionMap) session;
+     sessionMap = (SessionMap) session;   
     }
-
-//    public String doEmployeeAdd() throws SQLException {
-//        String result = "SUCCESS";
-//        EmployeeService.addEmployee(this);
-//        String createdMsg = "Employee created successfully!!";
-//        sessionMap.put("CreatedMsg", createdMsg);
-//        ArrayList empList = new ArrayList();
-//        empList = EmployeeService.getInstance().getAllEmployees();
-//        sessionMap.put("EmpList", empList);
-//        System.out.println("returning Success from doSearch method");
-//        result = "SUCCESS";
-//        return result;
-//    }
-
-//    public String doEmployeeUpdate() throws Exception {
-//
-//        String result = "FAILURE";
-//        boolean success = EmployeeService.updateEmployee(this);
-//
-//        if (success) {
-//            String updateMsg = "updated Employee";
-//            sessionMap.put("UpdateMsg", updateMsg);
-//
-//            ArrayList empList = EmployeeService.getInstance().getAllEmployees();
-//            sessionMap.put("EmpList", empList);
-//
-//            System.out.println("returning Success from doSearch method");
-//            result = "SUCCESS";
-//
-//        }
-//        return result;
-//    }
-//
-//    public String doEmployeeDelete() throws Exception {
-//
-//        String result = "FAILURE";
-//        boolean success = EmployeeService.deleteEmployee(this);
-//
-//        if (success) {
-//            String updateMsg = "deleted successfully!";
-//            sessionMap.put("UpdateMsg", updateMsg);
-//
-//            ArrayList empList = new ArrayList();
-//            empList = EmployeeService.getInstance().getAllEmployees();
-//            sessionMap.put("EmpList", empList);
-//
-//            result = "SUCCESS";
-//
-//        }
-//        return result;
-//    }
-//
+    
+    
+    
+    
+      
 //    public String doInventoryAdd() throws SQLException {
 //        String result = "SUCCESS";
 //        InvertoryService.addInventory(this);
@@ -111,8 +67,8 @@ public class Admin extends ActionSupport implements ApplicationAware, SessionAwa
 //        result = "SUCCESS";
 //        return result;
 //    }
-//
-//    public String doInventoryUpdate() throws SQLException {
+//    
+//        public String doInventoryUpdate() throws SQLException {
 //        String result = "SUCCESS";
 //        InvertoryService.updateInventory(this);
 //        String createdMsg = "Inventory created successfully!!";
@@ -120,12 +76,12 @@ public class Admin extends ActionSupport implements ApplicationAware, SessionAwa
 //        ArrayList invList = new ArrayList();
 //        invList = InventoryService.getInstance().getAllInventory();
 //        sessionMap.put("InvList", invList);
-//        System.out.println("returning Success from doSearch method");
+//        System.out.println("returning Success from doInventoryUpdate method");
 //        result = "SUCCESS";
 //        return result;
 //    }
-//
-//    public String doInventoryDelete() throws SQLException {
+//        
+//        public String doInventoryDelete() throws SQLException {
 //        String result = "SUCCESS";
 //        InvertoryService.deleteInventory(this);
 //        String createdMsg = "Inventory deleted!!";
@@ -137,6 +93,8 @@ public class Admin extends ActionSupport implements ApplicationAware, SessionAwa
 //        result = "SUCCESS";
 //        return result;
 //    }
+    
+    
 
     public String getEmailId() {
         return emailId;
@@ -170,4 +128,8 @@ public class Admin extends ActionSupport implements ApplicationAware, SessionAwa
         this.phoneNumber = phoneNumber;
     }
 
+   
+  
+    
+    
 }
